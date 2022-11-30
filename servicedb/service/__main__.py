c30a32e51b1d5348c50591122c2e743762a2aa96
alina1412 <alina1412@mail.ru>
2022-11-30 16:52:12 +0300

    +get_data endpoint, fixed pingdb, small edit

--- servicedb/service/__main__.py
+++ servicedb/service/__main__.py
@@ -1,14 +1,17 @@
-"""сервис записи в базу данных: servicedb/"""
+"""fastapi service to write to db: servicedb/"""
 
 import asyncio
 import logging
 
 import uvicorn
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
-from service.db.db_settings import get_session
-from service.rabbitmq_reseiver import from_que_to_db
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from service.db.db_settings import get_session  # isort: skip
+from service.rabbitmq_reseiver import from_que_to_db  # isort: skip
+from service.models import user_data  # isort: skip
 
 logger = logging.getLogger(__name__)
 
@@ -34,8 +37,26 @@ async def ping():
 async def pingdb(session: AsyncSession = Depends(get_session)):
     """ping db for debug"""
     query = "SELECT 1"
-    await session.execute(query).all()
+    (await session.execute(query)).all()
     return {"success": "Yes"}
+
+
+@app.get("/getdata")
+async def get_data(
+    perpage: int = Query(10, description="results per page"),
+    offset: int = Query(0, description="skip first N results"),
+    session: AsyncSession = Depends(get_session),
+):
+    """get data from db with pagination"""
+    query = (
+        select([user_data.c.id, user_data.c.name])
+        .order_by("id")
+        .limit(perpage)
+        .offset(offset)
+    )
+    res = (await session.execute(query)).fetchall()
+    res = list(res)
+    return {"success": str(res)}
 
 
 @app.on_event("startup")
